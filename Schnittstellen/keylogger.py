6059ba09b0d79467c830544b39544cc2b563848c
porkus1990 <porkus1990@googlemail.com>
2015-04-22 14:28:19 +0200

    Update keylogger.py

--- Schnittstellen/keylogger.py
+++ Schnittstellen/keylogger.py
@@ -4,21 +4,20 @@ import socket
 
 from msvcrt import getch  # try to import Windows version
 
+
+#ServeIP + Port zur Auswertung der Gesten
 UDP_IP = "141.82.50.192"
 UDP_PORT = 30000
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 	
 char = None
-
-def keypress():
-    global char
-    char = getch()
  
-thread.start_new_thread(keypress, ())
  
 while True:
+	#tastatureingabe
 	char = getch()
-	if char is not None:
-		sock.sendto(char, (UDP_IP, UDP_PORT)) #Befehl senden
+	
+	#befehl senden
+	sock.sendto(char, (UDP_IP, UDP_PORT)) #Befehl senden
 
 
